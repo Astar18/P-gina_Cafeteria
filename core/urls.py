93b20a2f8265a200b974af44b3857f82b2ed7c7b
Astar18 <62781005+Astar18@users.noreply.github.com>
2023-04-01 02:37:13 -0500

    primer commit

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),#root name siempre poner el nombre de la app
+    path('about/', views.about, name="about"),
+    path('store/', views.store, name="store"),
+]
